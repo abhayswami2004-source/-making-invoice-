3e86a3f09cca4d4fb0ddb6f083673e66bf84e732
abhayswami2004-source <abhayswami2004@gmail.com>
2025-12-18 22:06:49 +0530

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: "20px", fontFamily: "Arial" }}>
+      <h1>Simple Invoice App</h1>
+      <p>Welcome! This app will generate invoices.</p>
+    </main>
+  );
+}
